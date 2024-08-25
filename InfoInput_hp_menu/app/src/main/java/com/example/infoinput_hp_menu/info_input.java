ec7554e4418aacad210fcb5ae4c43c2670fd6ca9
yhnjiuy4321 <161418263+yhnjiuy4321@users.noreply.github.com>
2024-08-25 17:26:31 +0800

    update

--- InfoInput_hp_menu/app/src/main/java/com/example/infoinput_hp_menu/info_input.java
+++ InfoInput_hp_menu/app/src/main/java/com/example/infoinput_hp_menu/info_input.java
@@ -1,12 +1,19 @@
 package com.example.infoinput_hp_menu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class info_input extends AppCompatActivity {
 
@@ -21,4 +28,23 @@ public class info_input extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void submit(View view){
+
+   TextView name = findViewById(R.id.name);
+   TextView height = findViewById(R.id.input_h);
+   TextView weight = findViewById(R.id.input_w);
+   Spinner Activity = findViewById(R.id.spinner);
+
+    String user_name = name.getText().toString();
+    //把身高體重轉成int
+    int user_height = Integer.parseInt(height.getText().toString());
+    int user_weight = Integer.parseInt(weight.getText().toString());
+    //把運動量轉成int
+
+
+    }
+
+
+
 }
