e98f0b9f102fdffa4e6c6be3c01e389e37e837f6
陳韋翔 <161418419+D1204459@users.noreply.github.com>
2024-08-26 12:00:12 +0800

    001

--- InfoInput_hp_menu/app/build.gradle.kts
+++ InfoInput_hp_menu/app/build.gradle.kts
@@ -3,11 +3,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.infoinput_hp_menu"
+    namespace = "com.example.chen"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.infoinput_hp_menu"
+        applicationId = "com.example.chen"
         minSdk = 30
         targetSdk = 34
         versionCode = 1
